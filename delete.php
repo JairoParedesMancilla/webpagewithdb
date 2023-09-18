8a12f2ea47ffc574f28565a3cd07a1e76520c1f0
JairoParedesMancilla <143888494+JairoParedesMancilla@users.noreply.github.com>
2023-09-18 18:38:26 -0500

    Update delete.php

--- delete.php
+++ delete.php
@@ -9,8 +9,8 @@
         require "database/config.php";
         //Establish the connection
         $conn = mysqli_init();
-        mysqli_ssl_set($conn,NULL,NULL,$sslcert,NULL,NULL);
-        if(!mysqli_real_connect($conn, $host, $username, $password, $db_name, 3306, MYSQLI_CLIENT_SSL)){
+        mysqli_ssl_set($conn,NULL,NULL,NULL,NULL);
+        if(!mysqli_real_connect($conn, $host, $username, $password, $db_name, 3306)){
             die('Failed to connect to MySQL: '.mysqli_connect_error());
         }
 
